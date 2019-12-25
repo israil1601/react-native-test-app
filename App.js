c9d4d3b6e63f62a8860c58cafd75d13a339e24a0
Isroilkhon Salikhodjaev <isroilkhon.salikhodjaev@aalto.fi>
2019-12-25 18:33:45 +0200

    add previous video button

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, ScrollView} from 'react-native';
+import {StyleSheet, View, ScrollView, TouchableHighlight} from 'react-native';
 import YouTube from 'react-native-youtube';
 import PlayList from './components/playlist';
 import channels from './channels';
 import Header from './components/Header';
+import {Button} from 'react-native-elements';
 
 const App = () => {
   const [currentPlaylist, setPlaylist] = useState(channels[0].playlist);
@@ -41,6 +42,28 @@ const App = () => {
     }
   };
 
+  const previousVideoHandler = () => {
+    if (lastPlayedVideoIndex[currentChannelId] === 0) {
+      // if first video, move to last video
+      const previousVideo = currentPlaylist[currentPlaylist.length - 1]; // last video
+      setCurrentVideo(previousVideo);
+      setnextVideoId(currentPlaylist.length);
+      setLastPlayedVideoIndex({
+        ...lastPlayedVideoIndex,
+        [currentChannelId]: currentPlaylist.length - 1,
+      });
+    } else {
+      setCurrentVideo(
+        currentPlaylist[lastPlayedVideoIndex[currentChannelId] - 1],
+      );
+      setnextVideoId(nextVideoId - 1);
+      setLastPlayedVideoIndex({
+        ...lastPlayedVideoIndex,
+        [currentChannelId]: lastPlayedVideoIndex[currentChannelId] - 1,
+      });
+    }
+  };
+
   return (
     <View style={styles.screen}>
       <Header />
@@ -55,6 +78,16 @@ const App = () => {
           }
         }}
       />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Previous Video"
+          onPress={previousVideoHandler}
+          type="outline"
+          buttonStyle={styles.buttonStyle}
+          titleStyle={styles.titleStyle}
+          TouchableComponent={TouchableHighlight}
+        />
+      </View>
       <ScrollView>
         {channels.map(item => {
           return (
@@ -81,6 +114,18 @@ const styles = StyleSheet.create({
   video: {
     alignSelf: 'stretch',
     height: 300,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '90%',
+    borderWidth: 2,
+  },
+  buttonStyle: {
+    margin: 10,
+    borderColor: 'white',
+  },
+  titleStyle: {
+    color: 'white',
   },
 });
 
